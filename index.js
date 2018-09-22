4ad7d6c2b8fd30f1186e3d91b57fe1f012180d61
Bryan Rengo <bryan.rengo@gmail.com>
2018-09-21 20:10:58 -0500

    add start script

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express()
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('👋🌎'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`👋 ${port}!`))
