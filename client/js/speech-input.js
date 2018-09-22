35494a0d51163259c12a0041cd9c95ffa24ba2dc
jlandro <jaylandro@gmail.com>
2018-09-22 12:07:31 -0500

    Speech analysis addition

--- client/js/speech-input.js
+++ client/js/speech-input.js
@@ -12,20 +12,13 @@ if (!('webkitSpeechRecognition' in window)) {
     console.log('onstart: ',event);
   }
   recognition.onresult = function(event) {
-    var interim_transcript = '';
 
     for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
+      final_transcript += event.results[i][0].transcript;
     }
     
-    console.log('interim ', interim_transcript)
     console.log('final ', final_transcript)
     console.log(event.results)
-    emojiInput.innerHTML = interim_transcript;
     emojiInput.innerHTML = final_transcript;
   }
   recognition.onerror = function(event) {
